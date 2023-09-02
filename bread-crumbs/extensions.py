595b109b59e0d58345627d774ab6b4e23b801030
HongJinHyeok <vjinhyeokv@gmail.com>
2023-09-02 13:29:20 +0900

    Node와 Tree 클래스를 이용한 브레드크럼스 로직 구현
    [변경 내역]
    Node, Tree 클래스 생성
    Database 클래스에 get_connection 정적 메서드 생성
    HTTP 리소스 GET 요청 시 응답 데이터 모델 클래스 ResponseData 작성
    페이지 생성 코드 및 페이지 조회 코드 작성

---
+++ bread-crumbs/extensions.py
@@ -0,0 +1,11 @@
+from flask_restx import Api
+
+api = Api(
+  version='1.0',
+  title='Bread Crumbs',
+  description='Bread Crumbs API Server',
+  terms_url='/',
+  contact='redmax45@naver.com',
+  license='MIT',
+  doc='/'
+)
