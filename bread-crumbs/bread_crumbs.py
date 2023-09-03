e17d24760d23cb1d4eaa0f7cc009dc9b98badade
HongJinHyeok <vjinhyeokv@gmail.com>
2023-09-03 23:40:20 +0900

    주석 추가 및 테스트 케이스 데이터 변경
    [변경 내역]
    클래스 내 메서드 기능과 로직 주석 추가
    사용하지 않는 uuid 라이브러리 import문 삭제
    swagger 응답 코드 503번 추가

--- bread-crumbs/bread_crumbs.py
+++ bread-crumbs/bread_crumbs.py
@@ -2,7 +2,6 @@ from typing import List
 from flask_restx import Resource, Namespace
 from .bread_crumbs_api_models import res_model
 from . import config
-import uuid 
 import pymysql 
 
 
@@ -12,7 +11,7 @@ class Node:
     self.title = title # 제목 
     self.content = content # 내용 
     self.sub_page = [] # 서브 페이지 목록 
-    self.parent_page = None # 부모 페이지 
+    self.parent_page = None # 부모 페이지 Node: Node
   
   def add_sub_page(self, page):
     page.parent_page = self # 서브 페이지의 부모 페이지를 이 노드로 지정 
@@ -21,30 +20,40 @@ class Node:
   
 class Tree:
   def __init__(self):
+    # 아이디를 키로 갖고 노드를 밸류로 갖는 nodes 인스턴스 변수 생성 
     self.nodes = {}
     
+  # 트리가 가진 노드들 중 인수 아이디와 동일한 아이디를 가진 노드 반환
   def get_node(self, id) -> str:
     if id in self.nodes:
       return self.nodes[id]
   
-  def create_node(self, id: str, title: str, content: str) -> str: 
+  # 신규 노드 생성 
+  def create_node(self, id: str, title: str, content: str): 
+    # 현재 동일한 아이디를 가진 노드가 존재하지 않을 경우 노드 생성
     if id not in self.nodes:
       self.nodes[id] = Node(id, title, content)
-    return self.nodes[id]
-  
-  def connect_node(self, parent_page_id, sub_page_id):
+    
+  # 부모 페이지 노드와 자식 노드 페이지를 서로 연결
+  def connect_node(self, parent_page_id: str, sub_page_id: str):
+    # 노드에 부모 페이지 아이디와 서브 페이지 아이디가 존재할 경우 서로 연결    
     if parent_page_id in self.nodes and sub_page_id in self.nodes:
       self.nodes[parent_page_id].add_sub_page(self.nodes[sub_page_id])
   
-  def get_breadcrumbs(self, node):
+  # breadcrumbs 가져오기 
+  def get_breadcrumbs(self, node: Node):
+    # path 변수에 빈 리스트 할당 
     path = []
+    # 현재 노드부터 부모 페이지 노드까지 거슬러서 순회 
     while node:
       path.append(node)
       node = node.parent_page 
     
+    # 리스트에 현재 노드부터 추가되기 때문에 역순하여 반환 
     return path[::-1]
     
 
+# 데이터베이스 연결
 class Database:
   @staticmethod 
   def get_connection(config):
@@ -59,9 +68,10 @@ class Database:
     )
     return connection
 
+# 응답 데이터 형식 
 class ResponseData: 
-  def __init__(self, statusCode: int, id: str, title: str, content: str, sub_page: List, bread_crumbs: List):
-    self.statusCode = statusCode 
+  def __init__(self, id: str, title: str, content: str, sub_page: List, bread_crumbs: List):
+    # self.statusCode = statusCode 
     self.id = id 
     self.title = title 
     self.content = content 
@@ -70,16 +80,13 @@ class ResponseData:
     
   def to_dict(self):
     return {
-      'statusCode': self.statusCode, 
-      'id': self.id, 
-      'title': self.title, 
-      'content': self.content, 
-      'sub_page': self.sub_page, 
-      'bread_crumbs': self.bread_crumbs
+      # 'statusCode': self.statusCode, # 상태코드 
+      'id': self.id, # 페이지 아이디 
+      'title': self.title, # 페이지 제목
+      'content': self.content, # 페이지 내용
+      'sub_page': self.sub_page, # 서브 페이지 목록
+      'bread_crumbs': self.bread_crumbs # 페이지 경로
     }
-
-
-
 
 ns = Namespace("page")
 
@@ -88,11 +95,12 @@ ns = Namespace("page")
 class GetPage(Resource):
   @ns.doc(response={
     200: 'Success',
-    404: 'Page Not Found'
+    404: 'Page Not Found', 
+    503: "Interner Server Error"
   })
   @ns.marshal_with(res_model)
   @ns.doc(description="페이지를 조회합니다.")
-  def get(self, id) -> ResponseData:    
+  def get(self, id: str) -> ResponseData:    
     conn = None 
     tree = Tree()
     
@@ -102,34 +110,39 @@ class GetPage(Resource):
         query = """
         SELECT id, title, content, parent_page_id FROM PAGE        
         """
-        cursor.execute(query)
-        page_list = cursor.fetchall()
+        cursor.execute(query) # 쿼리 실행 
+        page_list = cursor.fetchall() # 전체 페이지 테이블 조회 결과 page_list에 저장 
         
+        # 1. page_list를 순회하면서 각 row마다 Node 생성
         for page in page_list:
           page_id = page['id']
           title = page['title']
           content = page['content']          
           tree.create_node(page_id, title, content)
         
+        # 2. 현재 페이지에 부모 페이지가 존재할 경우 현재 페이지 노드와 부모 페이지 노드를 서로 연결
         for page in page_list: 
           page_id = page['id']
           parent_page_id = page['parent_page_id']
           if parent_page_id:
             tree.connect_node(parent_page_id, page_id)
             
+      # 현재 조회하려는 노드를 변수에 저장             
       curr_node = tree.get_node(id)
+      # 만약 현재 조회하려는 노드가 존재하지 않는다면 404
       if not curr_node:        
-        response_data = ResponseData(404, None, None, None, None, None)
+        response_data = ResponseData(None, None, None, None, None)
         return response_data.to_dict(), 404    
       
       bread_crumbs = [node.id for node in tree.get_breadcrumbs(curr_node)]      
       sub_pages = [sub_page.id for sub_page in curr_node.sub_page]
       
-      response_data = ResponseData(200, curr_node.id, curr_node.title, curr_node.content, sub_pages, bread_crumbs)
-      return response_data.to_dict(), 200 
+      response_data = ResponseData(curr_node.id, curr_node.title, curr_node.content, sub_pages, bread_crumbs)
+      return response_data.to_dict(), 200
             
     except Exception as e:
-      return e 
+      response_data = ResponseData(None, None, None, None, None)
+      return response_data.to_dict(), 500
     finally:
       if conn: 
         conn.close()
@@ -140,17 +153,16 @@ class CreatePage(Resource):
   @ns.doc(responses={
     200: 'Success'
   })    
-  @ns.doc(description="테스트 데이터 저장")
-  def post(self) -> ResponseData:
-    # 5 -> 4 -> 2, 3 
+  @ns.doc(description="테스트 케이스 데이터 저장합니다.")
+  def post(self) -> int:
+    # 5 -> 4, 1 -> 2, 3 
     datas = [
-      {"id": 1, "title": "Title1", "content": "Content1", "parent_page_id": None},
-      {"id": 2, "title": "Title1", "content": "Content1", "parent_page_id": 4},      
-      {"id": 3, "title": "Title1", "content": "Content1", "parent_page_id": 4},
-      {"id": 4, "title": "Title1", "content": "Content1", "parent_page_id": 5},
       {"id": 5, "title": "Title1", "content": "Content1", "parent_page_id": None},
-    ]    
-    
+      {"id": 4, "title": "Title1", "content": "Content1", "parent_page_id": 5},      
+      {"id": 1, "title": "Title1", "content": "Content1", "parent_page_id": 5},      
+      {"id": 2, "title": "Title1", "content": "Content1", "parent_page_id": 4},      
+      {"id": 3, "title": "Title1", "content": "Content1", "parent_page_id": 4},                  
+    ]        
     conn = None
     try:
       conn = Database.get_connection(config)
@@ -165,8 +177,7 @@ class CreatePage(Resource):
       return 200
     except Exception as e:
       if conn:
-        conn.rollback()        
-      print(str(e))
+        conn.rollback()              
     finally:
       if conn:
         conn.close()    
